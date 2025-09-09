d4c2d3a03b58075f1f6c23d86207909767cb8b70
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-09 16:30:29 +0300

    fggfgggggg

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState();
-  const a = 0;
-  a = 3;
+  const [count, setCount] = useState(0);
 
   return (
     <>
