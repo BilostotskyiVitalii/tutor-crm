2e2db4a0a2d5f0162b1b8191be54157d7fc8e41b
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-09 15:20:36 +0300

    chore: update package.json with linting and formatting scripts, add ESLint and Prettier dependencies
    refactor: standardize import statements and fix minor issues in App.tsx and main.tsx

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      prettier,
+    },
+    rules: {
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      'no-shadow': 'off',
+      'no-console': 'warn',
+      'no-param-reassign': 'warn',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-no-useless-fragment': 'warn',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
-])
+];
