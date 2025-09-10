cabe1e38d38ae79b5158aef1202accfba31a3dc7
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-10 21:18:00 +0300

    feat: add header component with logo and update title in index.html

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,47 +1,25 @@
 import type { FC } from 'react';
-import { Layout, Menu, theme } from 'antd';
-import { UserOutlined, MailOutlined } from '@ant-design/icons';
+import { Avatar, Layout } from 'antd';
+import { UserOutlined, PaperClipOutlined } from '@ant-design/icons';
 
-import type { MenuProps } from 'antd';
+import styles from './HeaderComponent.module.scss';
 
 const { Header } = Layout;
 
-const headerItems: MenuItem[] = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'UserName',
-    key: 'user',
-    icon: <UserOutlined />,
-  },
-];
-
-type MenuItem = Required<MenuProps>['items'][number];
-
 const HeaderComponent: FC = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
   return (
-    <Header
-      style={{
-        position: 'sticky',
-        top: 0,
-        padding: 0,
-        background: colorBgContainer,
-      }}
-    >
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['1']}
-        items={headerItems}
-        style={{ flex: 1, minWidth: 0 }}
-      />
+    <Header className={styles.header}>
+      <div className={styles.logo}>
+        Clips CRM
+        <PaperClipOutlined />
+      </div>
+      <div>
+        LOGOUT
+        <Avatar
+          style={{ backgroundColor: '#8062b1ff', verticalAlign: 'middle' }}
+          icon={<UserOutlined />}
+        />
+      </div>
     </Header>
   );
 };
