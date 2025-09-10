bac9e4e2c59ebd1b7ee8a6b774ae7c38ce7bd1b5
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-10 19:34:39 +0300

    feat: add SiderComponent styles and update routing in AppRoutes

--- src/components/SiderComponent/SiderComponent.tsx
+++ src/components/SiderComponent/SiderComponent.tsx
@@ -1,5 +1,5 @@
-import { createElement, useState, type CSSProperties, type FC } from 'react';
-import { useNavigate } from 'react-router';
+import { createElement, useState, type FC } from 'react';
+import { useNavigate, useLocation } from 'react-router';
 
 import {
   BarChartOutlined,
@@ -8,23 +8,14 @@ import {
   HomeOutlined,
   ScheduleOutlined,
 } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
 
-import { Layout, Menu, theme } from 'antd';
-
-const siderStyle: CSSProperties = {
-  overflow: 'auto',
-  height: '100vh',
-  position: 'sticky',
-  insetInlineStart: 0,
-  top: 0,
-  bottom: 0,
-  scrollbarWidth: 'thin',
-  scrollbarGutter: 'stable',
-};
+import styles from './SiderComponent.module.scss';
 
 const SiderComponent: FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
   const { Sider } = Layout;
 
   const siderItems = [
@@ -34,33 +25,26 @@ const SiderComponent: FC = () => {
     { title: 'Analytics', icon: BarChartOutlined, path: '/analytics' },
     { title: 'Settings', icon: SettingFilled, path: '/settings' },
   ].map((item) => ({
-    key: item.title,
+    key: item.path,
     icon: createElement(item.icon),
     label: item.title,
-    path: item.path,
   }));
-
-  const handleClick = (e: { key: string }) => {
-    const item = siderItems.find((i) => i.key === e.key);
-    if (item) {
-      navigate(item.path);
-    }
-  };
 
   return (
     <Sider
-      style={siderStyle}
+      className={styles.sider}
       breakpoint="lg"
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
     >
+      <div className={styles.logo}>Tutor CRM</div>
       <Menu
         theme="dark"
         mode="inline"
         items={siderItems}
-        defaultSelectedKeys={['1']}
-        onClick={handleClick}
+        onClick={({ key }) => navigate(key)}
+        selectedKeys={[location.pathname.toLowerCase()]}
       />
     </Sider>
   );
