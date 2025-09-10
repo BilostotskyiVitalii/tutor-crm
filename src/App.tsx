31ef7f816a26207883618ee9a85d35fe40f17168
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-10 17:11:32 +0300

    feat: restructure App component and add routing with new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,21 @@
-import React, { type FC } from 'react';
+import { type FC } from 'react';
+import { Layout } from 'antd';
 
-import { UserOutlined, MailOutlined } from '@ant-design/icons';
-
-import type { MenuProps } from 'antd';
-import { Layout, Menu, theme } from 'antd';
-import SiderComponent from './components/SiderComponent/SiderComponent';
-
-const { Header, Content, Footer } = Layout;
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-const headerItems: MenuItem[] = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'UserName',
-    key: 'user',
-    icon: <UserOutlined />,
-  },
-];
+import {
+  AppRoutes,
+  SiderComponent,
+  HeaderComponent,
+  FooterComponent,
+} from '@/components';
 
 const App: FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   return (
     <Layout hasSider>
       <SiderComponent />
       <Layout>
-        <Header
-          style={{
-            position: 'sticky',
-            top: 0,
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            items={headerItems}
-            style={{ flex: 1, minWidth: 0 }}
-          />
-        </Header>
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div
-            style={{
-              padding: 24,
-              textAlign: 'center',
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            <p>long content</p>
-            {
-              // indicates very long content
-              Array.from({ length: 100 }, (_, index) => (
-                <React.Fragment key={index}>
-                  {index % 20 === 0 && index ? 'more' : '...'}
-                  <br />
-                </React.Fragment>
-              ))
-            }
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Tutor CRM ©{new Date().getFullYear()} Created with love by Vit
-        </Footer>
+        <HeaderComponent />
+        <AppRoutes />
+        <FooterComponent />
       </Layout>
     </Layout>
   );
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as SiderComponent } from './SiderComponent/SiderComponent';
+export { default as HeaderComponent } from './HeaderComponent/HeaderComponent';
+export { default as FooterComponent } from './FooterComponent/FooterComponent';
+export { default as AppRoutes } from './AppRoutes/AppRoutes';
