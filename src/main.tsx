2c417056163db857d62d6faf0fd3a10590746806
Vitalii Bilostotskyi <bilostotskyi1991@gmail.com>
2025-09-11 14:29:25 +0300

    feat: integrate Redux for state management and add authentication API

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.tsx';
+import { store } from '@/store/index.tsx';
 import './main.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
