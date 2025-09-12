1c375f35221a8a27236b4ee81fe1c1da396c9b98
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-09-12 12:44:08 +0300

    refactor: :art: worked on auth styles
    added registation component content. removed unnesaccery elements

--- src/constants/navigationUrls.ts
+++ src/constants/navigationUrls.ts
@@ -7,4 +7,4 @@ export const navigationUrls = {
   analytics: '/analytics',
   login: '/login',
   registration: '/registration',
-};
+} as const;
--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,5 +1,112 @@
-const RegistrationPage = () => {
-  return <h1>RegistrationPage</h1>;
+import { Button, Form, Input } from 'antd';
+import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router';
+import { navigationUrls } from '@/constants/navigationUrls';
+
+const RegistrationPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
+  function onLoginHandler(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    navigate(navigationUrls.login);
+  }
+
+  return (
+    <section className="auth-backdrop">
+      <Form
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        className="form"
+        scrollToFirstError
+      >
+        <h2 className="auth-form-title">Registration</h2>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input prefix={<MailOutlined />} placeholder="E-mail" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error('Passwords do not match!'));
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Confirm password"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="nickname"
+          tooltip="What do you want others to call you?"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your nickname!',
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Nickname" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Register
+          </Button>
+          or{' '}
+          <a href="" onClick={onLoginHandler}>
+            Log in!
+          </a>
+        </Form.Item>
+      </Form>
+    </section>
+  );
 };
 
 export default RegistrationPage;
