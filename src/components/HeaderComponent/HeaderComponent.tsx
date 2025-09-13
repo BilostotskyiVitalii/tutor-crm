eb4d0fc0d359aa44c55501c4dbc9c7afbd95421d
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-09-13 15:28:01 +0300

    refactor: :art: refactored header
    added UserMenu component

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,55 +1,19 @@
-import { useState, type FC } from 'react';
-import { Avatar, Dropdown, Layout, Space, type MenuProps } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { type FC } from 'react';
+import { Layout, Space } from 'antd';
 
-import { SelectLang, LogoComponent } from '@/components';
+import { SelectLang, LogoComponent, UserMenu } from '@/components';
 
 import styles from './HeaderComponent.module.scss';
 
 const { Header } = Layout;
 
-const items: MenuProps['items'] = [
-  {
-    label: '1st menu item',
-    key: '0',
-  },
-  {
-    type: 'divider',
-  },
-  {
-    label: 'Log out',
-    key: 'logout',
-  },
-];
-
 const HeaderComponent: FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleClick: MenuProps['onClick'] = ({ key }) => {
-    if (key === 'logout') {
-      console.log('Выходим из аккаунта');
-    }
-  };
-
-  function handleOpenChange(open: boolean) {
-    setIsOpen(open);
-  }
-
   return (
     <Header className={styles.header}>
       <LogoComponent />
       <Space>
         <SelectLang />
-        <Dropdown
-          menu={{ items, onClick: handleClick }}
-          trigger={['click']}
-          onOpenChange={handleOpenChange}
-        >
-          <Avatar
-            className={`${styles.avatar} ${isOpen ? styles.openMenu : ''}`}
-            icon={<UserOutlined />}
-          />
-        </Dropdown>
+        <UserMenu />
       </Space>
     </Header>
   );
---
+++ src/components/UserMenu/UserMenu.tsx
@@ -0,0 +1,48 @@
+import { useState, type FC } from 'react';
+import { Avatar, Dropdown, type MenuProps } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import styles from './UserMenu.module.scss';
+
+const items: MenuProps['items'] = [
+  {
+    label: '1st menu item',
+    key: '0',
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: 'Log out',
+    key: 'logout',
+  },
+];
+
+const UserMenu: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function handleOpenChange(open: boolean) {
+    setIsOpen(open);
+  }
+
+  const handleClick: MenuProps['onClick'] = ({ key }) => {
+    if (key === 'logout') {
+      console.log('Выходим из аккаунта');
+    }
+  };
+
+  return (
+    <Dropdown
+      menu={{ items, onClick: handleClick }}
+      trigger={['click']}
+      onOpenChange={handleOpenChange}
+    >
+      <Avatar
+        className={`${styles.avatar} ${isOpen ? styles.openMenu : ''}`}
+        icon={<UserOutlined />}
+      />
+    </Dropdown>
+  );
+};
+
+export default UserMenu;
