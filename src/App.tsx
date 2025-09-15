726beda064ff2943f213ac5aa1a9f7655f9417c0
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-09-15 18:48:52 +0300

    added useAuthListener hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import { Route, Routes } from 'react-router';
 import { LoginPage, RegistrationPage } from '@/pages';
 import { PrivateRoute, WorkPlaceComponent } from '@/components';
 import { navigationUrls } from '@/constants/navigationUrls';
+import { useAuthListener } from '@/hooks/useAuthListener';
 
 const App: FC = () => {
+  useAuthListener();
+
   return (
     <Routes>
       <Route path={navigationUrls.login} element={<LoginPage />} />
