94d636a888e3fc91303a6e970da18a8c78c20a7d
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-09-16 17:16:23 +0300

    fix: :bug: fixed persist bug

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userReducer from '@/store/userSlice';
 
@@ -14,8 +23,12 @@ export const store = configureStore({
   reducer: {
     user: persistedReducer,
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware().concat(authApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
