f97a01f18809acc989c4712c4f46b80d87161ef3
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-03 14:02:49 +0300

    added students info to lessons

--- src/features/lessons/components/LessonCard/LessonCard.tsx
+++ src/features/lessons/components/LessonCard/LessonCard.tsx
@@ -12,7 +12,7 @@ interface LessonCardProps {
 const LessonCard: FC<LessonCardProps> = ({ lesson, onEdit }) => {
   return (
     <div key={lesson.id} style={{ border: '1px solid' }}>
-      <h2>{lesson.studentIds}</h2>
+      <h2>{lesson.students.map((s) => s.name).join(', ')}</h2>
       <div>{lesson.notes}</div>
       <div>{`Start: ${lesson.start}`}</div>
       <div>{`End: ${lesson.end}`}</div>
