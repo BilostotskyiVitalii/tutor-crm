bd89d7c97667cb735798f7d623d767a69d46c5df
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-04 20:44:29 +0300

    refactored lessons

--- src/features/lessons/components/LessonFormModal/LessonFormModal.tsx
+++ src/features/lessons/components/LessonFormModal/LessonFormModal.tsx
@@ -1,16 +1,14 @@
-import { type FC, useEffect, useState } from 'react';
+import { type FC } from 'react';
 
 import { DatePicker, Form, Input, InputNumber, Modal } from 'antd';
-import dayjs from 'dayjs';
-import { Timestamp } from 'firebase/firestore';
 
 import { useGetGroupsQuery } from '@/features/groups/api/groupsApi';
 import { useGetLessonsQuery } from '@/features/lessons/api/lessonsApi';
-import LessonFormGroupSelect from '@/features/lessons/components/LessonFormModal/LessonFormGroupSelect';
-import UsersSelect from '@/features/lessons/components/LessonFormModal/LessonFormUsersSelect';
+import LessonFormGroupSelect from '@/features/lessons/components/LessonFormGroupSelect/LessonFormGroupSelect';
+import UsersSelect from '@/features/lessons/components/LessonFormUsersSelect/LessonFormUsersSelect';
 import { useLessonActions } from '@/features/lessons/hooks/useLessonActions';
+import { useLessonForm } from '@/features/lessons/hooks/useLessonForm';
 import type {
-  LessonData,
   LessonFormModalProps,
   LessonFormValues,
 } from '@/features/lessons/types/lessonTypes';
@@ -34,124 +32,30 @@ const LessonFormModal: FC<LessonFormModalProps> = ({
   const { data: lessons = [] } = useGetLessonsQuery();
   const { updateLessonData, createLesson } = useLessonActions();
   const { handleError } = useErrorHandler();
-  const [isGroup, setIsGroup] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    if (!isModalOpen) {
-      return;
-    }
-
-    form.resetFields();
-
-    if (editedLessonId) {
-      const lesson = lessons.find((l) => l.id === editedLessonId);
-      if (lesson) {
-        form.setFieldsValue({
-          studentIds: lesson.students.map((s) => s.id),
-          groupId: lesson.groupId,
-          date: [dayjs(lesson.start), dayjs(lesson.end)],
-          notes: lesson.notes || null,
-          price: lesson.price,
-        });
-        setIsGroup(!!lesson.groupId);
-        return;
-      }
-    }
-
-    if (defaultGroup) {
-      const group = groups.find((g) => g.id === defaultGroup);
-      if (group) {
-        form.setFieldsValue({
-          studentIds: group.studentIds,
-          groupId: group.id,
-          price: group.price,
-        });
-        setIsGroup(true);
-        return;
-      }
-    }
-
-    if (defaultStudent) {
-      const student = students.find((s) => s.id === defaultStudent);
-      form.setFieldsValue({
-        studentIds: [defaultStudent],
-        price: student?.price,
-      });
-      setIsGroup(false);
-    }
-  }, [
-    isModalOpen,
-    editedLessonId,
-    defaultGroup,
-    defaultStudent,
-    lessons,
-    groups,
-    students,
-    form,
-  ]);
-
-  const handleFinish = async () => {
-    setIsLoading(true);
-    try {
-      const formValues: LessonFormValues = await form.validateFields();
-
-      const lesson = editedLessonId
-        ? lessons.find((l) => l.id === editedLessonId)
-        : null;
-
-      const selectedStudents: { id: string; name: string; email: string }[] =
-        formValues.studentIds.map((id) => {
-          const studentFromList = students.find((s) => s.id === id);
-          if (studentFromList) {
-            const { id: sid, name, email } = studentFromList;
-            return { id: sid, name, email };
-          }
-
-          // Если студента нет в базе, берём его из урока
-          const studentFromLesson = lesson?.students.find((s) => s.id === id);
-          if (studentFromLesson) {
-            const { id: sid, name, email } = studentFromLesson;
-            return { id: sid, name, email };
-          }
-
-          // Если нигде нет, создаём "заглушку"
-          return { id, name: id, email: '' };
-        });
-
-      const reqValues: LessonData = {
-        students: selectedStudents,
-        groupId: formValues.groupId || null,
-        start: Timestamp.fromMillis(formValues.date[0].valueOf()),
-        end: Timestamp.fromMillis(formValues.date[1].valueOf()),
-        notes: formValues.notes || null,
-        price: formValues.price,
-      };
-
-      if (editedLessonId) {
-        await updateLessonData(editedLessonId, reqValues);
-      } else {
-        await createLesson(reqValues);
-      }
-      onClose();
-      form.resetFields();
-    } catch (err) {
-      handleError(err, 'Lesson form error');
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  function handleCancel() {
-    onClose();
-    form.resetFields();
-  }
+  const { isGroup, setIsGroup, isLoading, handleFinish, handleCancel } =
+    useLessonForm({
+      form,
+      editedLessonId,
+      defaultStudent,
+      defaultGroup,
+      students,
+      lessons,
+      groups,
+      updateLessonData,
+      createLesson,
+      handleError,
+      onClose,
+    });
 
   return (
     <Modal
       title={editedLessonId ? 'Edit Lesson' : 'Create Lesson'}
       open={isModalOpen}
-      onCancel={handleCancel}
+      onCancel={() => {
+        handleCancel();
+        onClose();
+      }}
       onOk={handleFinish}
       okText={editedLessonId ? 'Update' : 'Create'}
       cancelText="Cancel"
