3c43659f85f828bb25380bbc1d258792f6740ac5
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-04 23:26:03 +0300

    replace add lesson button to header

--- src/shared/components/Layout/HeaderComponent/HeaderComponent.tsx
+++ src/shared/components/Layout/HeaderComponent/HeaderComponent.tsx
@@ -1,8 +1,11 @@
-import { type FC } from 'react';
+import { type FC, useState } from 'react';
 
 import { MenuOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import { Button, Layout, Space } from 'antd';
 
+import LessonFormModal from '@/features/lessons/components/LessonFormModal/LessonFormModal';
+import type { ModalState } from '@/features/lessons/types/lessonTypes';
 import UserMenu from '@/features/user/components/UserMenu/UserMenu';
 import LogoComponent from '@/shared/components/Layout/LogoComponent/LogoComponent';
 import SelectLang from '@/shared/components/UI/SelectLang/SelectLang';
@@ -17,6 +20,12 @@ type HeaderProps = {
 };
 
 const HeaderComponent: FC<HeaderProps> = ({ onBurgerClick, isMobile }) => {
+  const [modalState, setModalState] = useState<ModalState>(null);
+  const closeModal = () => setModalState(null);
+  const openLessonModal = (lessonId: string | null = null) => {
+    setModalState({ type: 'lesson', lessonId });
+  };
+
   return (
     <Header className={styles.header}>
       {isMobile && onBurgerClick && (
@@ -29,9 +38,23 @@ const HeaderComponent: FC<HeaderProps> = ({ onBurgerClick, isMobile }) => {
       )}
       <LogoComponent />
       <Space>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => openLessonModal()}
+        >
+          New lesson
+        </Button>
         <SelectLang />
         <UserMenu />
       </Space>
+      {modalState?.type === 'lesson' && (
+        <LessonFormModal
+          isModalOpen
+          onClose={closeModal}
+          editedLessonId={modalState?.lessonId}
+        />
+      )}
     </Header>
   );
 };
