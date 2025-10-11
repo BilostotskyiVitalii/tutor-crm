c611c07a1c845923ad03a2ac5f70ea990dc8a549
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-11 13:12:06 +0300

    replace content component

--- src/routes/AppRoutes/AppRoutes.tsx
+++ src/routes/AppRoutes/AppRoutes.tsx
@@ -1,11 +1,9 @@
 import { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Layout, Spin } from 'antd';
+import { Spin } from 'antd';
 
 import { navigationUrls } from '@/shared/constants/navigationUrls';
-
-import styles from './AppRoutes.module.scss';
 
 const DashboardPage = lazy(() => import('@/pages/DashboardPage/DashboardPage'));
 const StudentsPage = lazy(() => import('@/pages/StudentsPage/StudentsPage'));
@@ -15,31 +13,27 @@ const SettingsPage = lazy(() => import('@/pages/SettingsPage/SettingsPage'));
 const AnalyticsPage = lazy(() => import('@/pages/AnalyticsPage/AnalyticsPage'));
 const GroupsPage = lazy(() => import('@/pages/GroupsPage/GroupsPage'));
 
-const { Content } = Layout;
-
 export function AppRoutes() {
   return (
-    <Content className={styles.content}>
-      <Suspense fallback={<Spin fullscreen size="large" />}>
-        <Routes>
-          <Route path={navigationUrls.dashboard} element={<DashboardPage />} />
-          <Route path={navigationUrls.students} element={<StudentsPage />} />
-          <Route path={navigationUrls.student} element={<StudentIdPage />} />
-          <Route path={navigationUrls.schedule} element={<SchedulePage />} />
-          <Route path={navigationUrls.settings} element={<SettingsPage />} />
-          <Route path={navigationUrls.analytics} element={<AnalyticsPage />} />
-          <Route path={navigationUrls.groups} element={<GroupsPage />} />
+    <Suspense fallback={<Spin fullscreen size="large" />}>
+      <Routes>
+        <Route path={navigationUrls.dashboard} element={<DashboardPage />} />
+        <Route path={navigationUrls.students} element={<StudentsPage />} />
+        <Route path={navigationUrls.student} element={<StudentIdPage />} />
+        <Route path={navigationUrls.schedule} element={<SchedulePage />} />
+        <Route path={navigationUrls.settings} element={<SettingsPage />} />
+        <Route path={navigationUrls.analytics} element={<AnalyticsPage />} />
+        <Route path={navigationUrls.groups} element={<GroupsPage />} />
 
-          <Route
-            path=""
-            element={<Navigate to={navigationUrls.dashboard} replace />}
-          />
-          <Route
-            path="*"
-            element={<Navigate to={navigationUrls.dashboard} replace />}
-          />
-        </Routes>
-      </Suspense>
-    </Content>
+        <Route
+          path=""
+          element={<Navigate to={navigationUrls.dashboard} replace />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to={navigationUrls.dashboard} replace />}
+        />
+      </Routes>
+    </Suspense>
   );
 }
