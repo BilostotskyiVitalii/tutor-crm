5ccb4d9558b38351b78d9fd62ad08c7912bceaa5
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-13 13:23:25 +0300

    added useGroupForm hook

---
+++ src/features/groups/hooks/useGroupForm.ts
@@ -0,0 +1,73 @@
+import { useEffect, useMemo, useState } from 'react';
+
+import { Form } from 'antd';
+
+import { useGetGroupsQuery } from '@/features/groups/api/groupsApi';
+import { useGroupActions } from '@/features/groups/hooks/useGroupActions';
+import type { GroupData } from '@/features/groups/types/groupTypes';
+import { useGetStudentsQuery } from '@/features/students/api/studentsApi';
+import { useErrorHandler } from '@/shared/hooks/useErrorHandler';
+
+interface useGroupFormProps {
+  groupId?: string | null;
+  onClose: () => void;
+}
+
+export const useGroupForm = ({ groupId, onClose }: useGroupFormProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [form] = Form.useForm<GroupData>();
+  const { data: students = [] } = useGetStudentsQuery();
+  const { data: groups = [] } = useGetGroupsQuery();
+  const { createGroup, updateGroupData } = useGroupActions();
+  const { handleError } = useErrorHandler();
+
+  const group = useMemo(
+    () => (groupId ? groups.find((g) => g.id === groupId) : null),
+    [groupId, groups],
+  );
+
+  useEffect(() => {
+    if (group) {
+      // TODO Why notes separetly?
+      form.setFieldsValue({ ...group, notes: group?.notes ?? null });
+    }
+  }, [group, form]);
+
+  const studentOptions = useMemo(
+    () =>
+      students
+        .filter((s) => s.isActive)
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((s) => ({ label: s.name, value: s.id })),
+    [students],
+  );
+
+  const onFinish = async () => {
+    try {
+      setIsLoading(true);
+      const formValues: GroupData = await form.validateFields();
+      const normalizedFormValues = {
+        ...formValues,
+        notes: formValues.notes?.trim() || null,
+      };
+
+      if (groupId) {
+        await updateGroupData(groupId, normalizedFormValues);
+      } else {
+        await createGroup(normalizedFormValues);
+      }
+
+      onClose();
+    } catch (err) {
+      handleError(err, 'Group form error!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return {
+    form,
+    onFinish,
+    studentOptions,
+    isLoading,
+  };
+};
