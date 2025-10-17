d9221d0271ae6d8f141b6fbc734a0486a8b80c1a
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-17 13:10:27 +0300

    basic layout dashboard

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -2,16 +2,11 @@ import type { FC } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import {
-  DollarOutlined,
-  GroupOutlined,
-  TrophyOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import { GroupOutlined, TrophyOutlined, UserOutlined } from '@ant-design/icons';
 import {
   Alert,
-  Avatar,
   Card,
+  Divider,
   Flex,
   List,
   Segmented,
@@ -23,11 +18,14 @@ import {
 import { useGetDashboardStatsQuery } from '@/features/dashboard/api/dashboardApi';
 import AvatarCustom from '@/shared/components/UI/AvatarCustom/AvatarCustom';
 
+import styles from './DashboardPage.module.scss';
+
 const { Title, Text } = Typography;
 
 const DashboardPage: FC = () => {
   const { data, isLoading, isError } = useGetDashboardStatsQuery();
   const [topMode, setTopMode] = useState<'hours' | 'income'>('hours');
+
   const topList =
     topMode === 'hours'
       ? data?.topStudentsByHours || []
@@ -39,28 +37,15 @@ const DashboardPage: FC = () => {
 
   return (
     <Spin spinning={isLoading}>
-      <Flex wrap gap="large" justify="start" align="stretch">
-        {/* Students */}
-        <Card title="👩‍🎓 Students" style={{ width: 280 }}>
-          <Statistic
-            title="Active students"
-            value={data?.activeStudents}
-            prefix={<UserOutlined />}
-            valueStyle={{ color: '#3f8600' }}
-          />
-          <Statistic
-            title="New this month"
-            value={data?.newStudents}
-            prefix={<UserOutlined />}
-            valueStyle={{ color: '#52c41a' }}
-          />
-        </Card>
-
-        {/* Top Students */}
-        <Card
-          style={{ minWidth: 400 }}
-          title={
-            <Flex align="center" justify="space-between">
+      {/* Top Students */}
+      <Flex gap={12} wrap>
+        <div className={styles.topstudentsWrapper}>
+          <Card className={styles.topStudents}>
+            <Flex
+              align="center"
+              justify="space-between"
+              style={{ marginBottom: 16 }}
+            >
               <Flex align="center" gap="small">
                 <TrophyOutlined style={{ color: '#faad14' }} />
                 <Title level={5} style={{ margin: 0 }}>
@@ -77,91 +62,125 @@ const DashboardPage: FC = () => {
                 ]}
               />
             </Flex>
-          }
-        >
-          <List
-            // itemLayout="horizontal"
-            dataSource={topList}
-            renderItem={(item, index) => (
-              <List.Item>
-                <Flex gap="middle" style={{ width: '100%' }}>
-                  <Text style={{ fontSize: 30 }}>
-                    {index === 0 ? '🥇' : index === 1 ? '🥈' : '🥉'}
-                  </Text>
-                  <List.Item.Meta
-                    style={{ display: 'flex', alignItems: 'center' }}
-                    avatar={<AvatarCustom src={item.avatar} name={item.name} />}
-                    title={<Link to={`/students/${item.id}`}>{item.name}</Link>}
-                    description={
-                      topMode === 'hours' ? (
-                        <Text type="secondary">
-                          {item.totalHours.toFixed(0)} h
-                        </Text>
-                      ) : (
-                        <Text type="secondary">
-                          ${item.totalRevenue.toFixed(0)}
-                        </Text>
-                      )
-                    }
-                  />
-                </Flex>
-              </List.Item>
-            )}
-          />
-        </Card>
 
-        {/* Groups */}
-        <Card title="👥 Groups" style={{ width: 280 }}>
-          <Statistic
-            title="Active groups"
-            value={data?.activeGroups}
-            prefix={<GroupOutlined />}
-          />
-        </Card>
+            <List
+              dataSource={topList}
+              renderItem={(item, index) => (
+                <List.Item>
+                  <Flex gap="middle" style={{ width: '100%' }}>
+                    <Text style={{ fontSize: 28 }}>
+                      {index === 0 ? '🥇' : index === 1 ? '🥈' : '🥉'}
+                    </Text>
+                    <Divider
+                      type="vertical"
+                      style={{ height: '64px', margin: '0 12px' }}
+                    />
+                    <List.Item.Meta
+                      style={{ display: 'flex', alignItems: 'center' }}
+                      avatar={
+                        <AvatarCustom src={item.avatar} name={item.name} />
+                      }
+                      title={
+                        <Link to={`/students/${item.id}`}>{item.name}</Link>
+                      }
+                      description={
+                        topMode === 'hours' ? (
+                          <Text type="secondary">
+                            {item.totalHours.toFixed(0)} h
+                          </Text>
+                        ) : (
+                          <Text type="secondary">
+                            ${item.totalRevenue.toFixed(0)}
+                          </Text>
+                        )
+                      }
+                    />
+                  </Flex>
+                </List.Item>
+              )}
+            />
+          </Card>
+        </div>
+        <div className={styles.masonryContainer}>
+          {/* Students */}
+          <Card className={`${styles.masonryItem} ${styles.students}`}>
+            <Statistic
+              title="Active"
+              value={data?.activeStudents}
+              prefix={<UserOutlined />}
+              valueStyle={{ color: '#3f8600' }}
+            />
+          </Card>
+          <Card className={`${styles.masonryItem} ${styles.students}`}>
+            <Statistic
+              title="New"
+              value={data?.newStudents}
+              prefix={<UserOutlined />}
+              valueStyle={{ color: '#52c41a' }}
+            />
+          </Card>
 
-        {/* Lessons */}
-        <Card title="📘 Lessons" style={{ width: 280 }}>
-          <Statistic title="Done this month" value={data?.doneLessons} />
-          <Statistic title="Planned this month" value={data?.plannedLessons} />
-        </Card>
+          {/* Groups */}
+          <Card className={`${styles.masonryItem} ${styles.groups}`}>
+            <Statistic
+              title="Active groups"
+              value={data?.activeGroups}
+              prefix={<GroupOutlined />}
+            />
+          </Card>
 
-        {/* Income */}
-        <Card title="💰 Month income" style={{ width: 280 }}>
-          <Statistic
-            title="Current"
-            value={data?.currentMonthIncome}
-            prefix="$"
-            precision={0}
-          />
-          <Statistic
-            title="Expected"
-            value={data?.expectedMonthIncome}
-            prefix="$"
-            precision={0}
-          />
-        </Card>
+          {/* Lessons */}
+          <Card className={`${styles.masonryItem} ${styles.lessons}`}>
+            <Statistic title="Done" value={data?.doneLessons} />
+          </Card>
+          <Card className={`${styles.masonryItem} ${styles.lessons}`}>
+            <Statistic title="Planned" value={data?.plannedLessons} />
+          </Card>
 
-        {/* Average Prices */}
-        <Card title="📈 Average price" style={{ width: 280 }}>
-          <Statistic
-            title="Per lesson"
-            prefix="$"
-            precision={0}
-            value={data?.avgLessonPrice}
-          />
-          <Statistic
-            title="Per hour"
-            prefix="$"
-            precision={0}
-            value={data?.averageHourPrice}
-          />
-          <Statistic
-            title="Per student/hour"
-            prefix="$"
-            precision={0}
-            value={data?.averagePerHourStudentPrice}
-          />
-        </Card>
+          {/* Income */}
+          <Card className={`${styles.masonryItem} ${styles.income}`}>
+            <Statistic
+              title="Current"
+              value={data?.currentMonthIncome}
+              prefix="$"
+              precision={0}
+            />
+          </Card>
+          <Card className={`${styles.masonryItem} ${styles.income}`}>
+            <Statistic
+              title="Expected"
+              value={data?.expectedMonthIncome}
+              prefix="$"
+              precision={0}
+            />
+          </Card>
+
+          {/* Average Prices */}
+          <Card className={`${styles.masonryItem} ${styles.prices}`}>
+            <Statistic
+              title="Lesson"
+              prefix="$"
+              precision={0}
+              value={data?.avgLessonPrice}
+            />
+          </Card>
+          <Card className={`${styles.masonryItem} ${styles.prices}`}>
+            <Statistic
+              title="Hour"
+              prefix="$"
+              precision={0}
+              value={data?.averageHourPrice}
+            />
+          </Card>
+          <Card className={`${styles.masonryItem} ${styles.prices}`}>
+            <Statistic
+              title="Student per/hour"
+              prefix="$"
+              precision={0}
+              value={data?.averagePerHourStudentPrice}
+            />
+          </Card>
+        </div>
       </Flex>
     </Spin>
   );
