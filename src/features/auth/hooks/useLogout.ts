35f9dd0437d2015d47023faf0235788c9764407a
Vitalii Bilostotskyi <vitalii.bilostotskyi@gmail.com>
2025-10-30 16:00:03 +0200

    added apis reset on logout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,6 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 
 import { authApi } from '@/features/auth/api/authApi';
+import { dashboardApi } from '@/features/dashboard/api/dashboardApi';
+import { groupsApi } from '@/features/groups/api/groupsApi';
+import { lessonsApi } from '@/features/lessons/api/lessonsApi';
+import { studentsApi } from '@/features/students/api/studentsApi';
 import { useAppDispatch } from '@/store/reduxHooks';
 
 export const useLogout = () => {
@@ -10,6 +14,10 @@ export const useLogout = () => {
   const logout = () => {
     localStorage.removeItem('token');
     dispatch(authApi.util.resetApiState());
+    dispatch(studentsApi.util.resetApiState());
+    dispatch(lessonsApi.util.resetApiState());
+    dispatch(groupsApi.util.resetApiState());
+    dispatch(dashboardApi.util.resetApiState());
     navigate('/login');
   };
 
